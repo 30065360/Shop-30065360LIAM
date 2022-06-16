f47ab755b3ebd3104cac7a0ec730b3ce6ff19d1e
i am jake <101292952+30065360@users.noreply.github.com>
2022-06-16 21:05:16 +1200

    Initializing github repository

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_30065360LIAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
